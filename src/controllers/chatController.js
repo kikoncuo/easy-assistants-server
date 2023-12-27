e11cd71b2f62a3f51db5d022a05421f3604ac464
Enrique <enriqueag26@gmail.com>
2023-12-27 08:47:25 +0100

    Refactor chatController.js to save chat data to Supabase and handle assistant's response

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -7,66 +7,72 @@ const isSupabaseEnabled = process.env.SUPABASE_URL && process.env.SUPABASE_KEY;
 let supabase;
 if (isSupabaseEnabled) {
   supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+  logger.log('info', 'Supabase client initialized, chat data will be saved to Supabase');
 }
 
-async function saveChatDataToSupabase(threadId, userQuery, toolOutputs, responseContent) {
-  // Prepare the messages array to be inserted into Supabase
+async function saveChatDataToSupabase(userQuery, toolOutputs, response) {
   const messages = [];
+  const threadId = response.threadId;
 
-  // Add user query to messages array only if it's a regular query (not a tool function call)
   if (!toolOutputs || !toolOutputs.calls || toolOutputs.calls.length === 0) {
-    messages.push({ role: 'user', content: userQuery });
+    messages.push({ thread_id: threadId||1, role: 'user', content: userQuery });
   }
 
-  // Process the tool function responses
   if (toolOutputs && toolOutputs.calls && toolOutputs.calls.length > 0) {
     for (const call of toolOutputs.calls) {
       const functionDetails = call.function;
       if (functionDetails && functionDetails.name) {
-        // Add function response to messages array
-        messages.push({ role: 'function', name: functionDetails.name, content: functionDetails.arguments });
-      }
-    }
-  }
-
-  // Process the assistant's response content
-  if (responseContent && responseContent.length > 0) {
-    for (const content of responseContent) {
-      if (content.type === 'text') {
-        // Add assistant's text response to messages array
-        messages.push({ role: 'assistant', content: content.text.value });
-      } else if (content.type === 'function_call') {
-        // Add assistant's function call to messages array
         messages.push({
-          role: 'assistant',
-          function_call: {
-            name: content.function_call.name,
-            arguments: JSON.stringify(content.function_call.arguments)
-          }
+          thread_id: threadId,
+          role: 'function',
+          content: JSON.stringify(functionDetails.arguments),
+          function_name: functionDetails.name,
+          function_arguments: functionDetails.arguments
         });
       }
     }
   }
 
-  // Save messages to Supabase if client is initialized
+  if (response) {
+      if (response.responseContent === 'text'|| response.responseContent[0].type === 'text') {
+        messages.push({ thread_id: threadId, role: 'assistant', content: response.responseContent[0].text.value });
+      } else if (response.responseContent === 'requires_action') {
+        // for each call in response.tools.calls store a message
+        for (const call of response.tools.calls) {
+          const functionDetails = call.function;
+          if (functionDetails && functionDetails.name) {
+            messages.push({
+              thread_id: threadId,
+              role: 'assistant',
+              content: 'function_call',
+              function_name: functionDetails.name,
+              function_arguments: functionDetails.arguments
+            });
+          }
+        }
+      }
+  }
+
   if (isSupabaseEnabled) {
-    await supabase.from('chat_data').insert({ thread_id: threadId, messages });
+    const storeRes = await supabase.from('chat_data').insert(messages);
+    if (storeRes.error) {
+        logger.log('error', JSON.stringify(storeRes, null, 2));
+    }
   }
 }
 
 async function handleChatRequest(req, res) {
   try {
     const { query: userQuery, threadId, tool_outputs: toolOutputs, instructions } = req.body;
+    
     const response = await openaiService.getAssistantResponse(userQuery, threadId, toolOutputs, instructions);
 
-    // Save chat data to Supabase if enabled
     if (isSupabaseEnabled) {
-      await saveChatDataToSupabase(threadId, userQuery, toolOutputs, response.responseContent);
+      await saveChatDataToSupabase(userQuery, toolOutputs, response);
     }
 
     res.json(response);
   } catch (error) {
-    // Log the error with or without threadId based on its availability
     const threadId = req.body.threadId;
     logger.logWithThreadId('error', `Error in AI chat: ${error}`, threadId || null);
     res.status(500).send('Error processing chat request');
