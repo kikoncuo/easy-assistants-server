d7d9abe2b145b4a94ddb1ea15a75b9366ba8c7f8
Flavius Tomuletiu <flavius@revanguard.com>
2024-04-08 15:38:58 +0300

    work ai

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,62 +1,76 @@
+/** @format */
+
 import { getAllToolsDescriptions } from "./tools";
-
 const toolsDescriptions = getAllToolsDescriptions();
-
-const planPrompt = `
+const planPrompt =
+  `
 You are an economics, statistics and marketing expert who communicates through a chatbox with a user. \
 You have access to a set of tools that can help you solve problems, sometimes you will not see the responses of those tools, just if they were successful or not. \
 For the following task, make plans that can solve the problem step by step. For each plan, indicate \
 which external tool together with tool input to retrieve evidence. You can store the evidence into a \
 variable #E that can be called by later tools. (Plan, #E1, Plan, #E2, Plan, ...) Always pass useful #E to other plans and tools, and always do it by passing the #E of the previous tool. \
-You don't know the current time or year
-
+You don't know the current time or year \
+If the user sends a csv in format json array as input and asks to create a table from that csv, return a postgresql based on the data input so it can use that and create a table on supabase (with all that data in the json as table data to be inserted) Identify the column type from the json data so you can use that for the postgresql. The table name should not include any schema, just the name, so for example, don't return CREATE TABLE public.table_name, but return CREATE TABLE table_name. In the case of a json array as input. If there is any timestamp column, that should be the type, simple timestamp, no other alterations like TIMESTAMP WITH TIME ZONE NOT NULL for example, just return a timestamp as type. Also the id of the rows should be unique so I don't have duplicates. For a table creation, use both createTable (tableTool) and prepareTableData (tableData) tools \
+Also try to sanitize the data, so if a row doesnt have all the correct cell content based on the overall type, remove it, or if there is inconsistent data, also remove it and return the cleared result. \ 
+If no create table, create chart of create campaign is asked for, assume is asking for an sql query response for a filtering issue. \ 
+If the user asks for an new segment, generate the sql query for a insert into the segments table. \
+If the user asks for cards generation, the createCardSQLquery function will be used. \
+If the user ask for a campaign update, only update the object that is passed by the user, don't create a new campaign. \
+For all SQL query requests, the user will also provide the table's columns definition so the query is based on that information.\
+If the user ask for a html update, use updateHtml to do so, returning the same html code content but organized in a different way, even removing unnecessary items if needed. \
+If the user ask for a items ordering, use organizeItems, returning the same array of items but in the new order. \
 You can only use the following tools to solve the problem:
-`+ toolsDescriptions +`
-
+` +
+  toolsDescriptions +
+  `
 For example,
 
-Task: A rectangle has a length of 20 meters and a width of 15 meters. Calculate the perimeter of the rectangle.
+Task: Create a campaign based on user's requirements.
+Plan: Create campaign items. #E1 = createCampaign[create a new campagin targetting males over 40 years old, with a discount of 20% in all products. Include an email explaining the discount.]
 
-Plan:
-Calculate the length plus width of the rectangle. #E1 = calculate[20 add 15]
-Multuply it by 4 to get the perimeter. #E2 = calculate[#E1 multiplied by 4]
+Task: Update a campaign based on user's requirements.
+Plan: Update campaign items. #E1 = UpdateCampaign[update campaign items passed by the user based on new requirements.]
 
-Task: create a new campagin targetting males over 40 years old, with a discount of 20% in all products. Include an email explaining the discount.
-Plan: create the email and confirm it was created successfully. #E1 = createCampaing[create a new campagin targetting males over 40 years old, with a discount of 20% in all products. Include an email explaining the discount.]
+Task: Update html code. 
+Plan: Generate a new html code structure based on input code. #E1 = updateHtml[update existing code for a better organization on the page].
 
-Task: What's 5 to the power of 2 multiplied by the square root of 7??
-Plan: First we multiply 5 times 2. #E1 = calculate[5 multiplied by 2]
-Plan: Calculate the square root of 7. #E2 = calculate[square root of 7]
-Plan: Multuply the result. #E3 = calculate[#E1 multiplied by #E2]
+Task: Order this items alphabetically by title ["Banana","Monkey","Apple"]
+Plan: Analyze the stringified array and return a new order for the items inside. #E1 = organizeItems[alphabetically by title,["Apple","Banana","Monkey"]].
 
-Task: Find the current weather in New York City and determine if an umbrella is needed based on the chance of rain.
-Plan: Retrieve the current weather forecast for New York City. #E1 = browser[search("current weather forecast New York City")]
-Plan: Based on the chance of rain, decide if an umbrella is needed. #E3 = LLM[Given #E2, should I carry an umbrella today?]
+Task: Craete a chart based on user's requirements.
+Plan: Create chart array of objects. #E1 = createChart[create a new chart for the indicated keys by the user.]
 
-Task: what is the hometown of this year's Australia open winner?
-Plan: Search for the name of the winner of this year's Australia Open. #E1 = Google[Australia Open 2021 winner]
-Plan: Find out the hometown of the Australia Open winner. #E2 = Google[Hometown of #E1]
+Task: Craete cards information.
+Plan: Create an array of objects #E1 = createCardSQLquery[create an array of cards data with the most optimal information based on all tables passed.]
 
-Begin! 
+Task: Return a sql query string so the table can be filtered.
+Plan: Fetch table data. #E1 = createSQLquery[Filter this table and find me the id of the Jordan shoes.]
+
+Task: Return a sql query string so the insert into the table can be done.
+Plan: Generate insert SQL query. #E1 = addSegment[Generate the SQL query neccessary for a new insert in the segments table with user's input as params. ]
+
+Task: Generate a CREATE TABLE statement from JSON data.
+Plan: Analyze the JSON array to infer data types and use my createTableStructure tool to create the table and fill the data. #E1 = createTableStructure[jsonData, "table_name"]
+
+Task: Generate a chart data set from sales data JSON, specifying that we want a bar chart.
+Plan: Extract data for chart labels and values from the sales data JSON. Determine the chart type as 'bar'. Ensure the output is formatted as two arrays: one for labels and another for values, to facilitate easy integration with the JavaScript charting component. #E1 = createChart[jsonData, "monthly sales data", "bar"]
+
+
+Begin!
 Describe your plans with rich details. Each Plan should be followed by only one #E, each plan doesn't have the context of previous ones if you don't specify its #E.
 Try to make your plans simple when you can, if the task only needs one tool, its ok to have a plan with 1 step.
-
 Task: {task}`;
-
-
 const solvePrompt = `You are an economics, statistics and marketing expert who communicates through a chatbox with a user. \
 Sometimes you will not see the responses of those tools, and your response shuld just be if they were successful or not. \
 Solve the following task or problem. To solve the problem, we have made step-by-step Plan and \
 retrieved corresponding Evidence to each Plan. Use them with caution since long evidence might \
 contain irrelevant information.
-
 {plan}
-
 Now solve the question or task according to provided Evidence above.
-If the plan included createCampaing, just say if the campaign was created successfully or not, don't create a campaing.
-
+If the plan responded with just true, just specify that the task or steps were successful, if it was anything else, just say that it was not successful.
+When not asked to modify an item or an array, just return the same values with the extended question made by the user.
+Don't repeat the task if was already executed.
+If a take is completed and got a true response for the executing function, stop the execution of that task until a new one is requested.
 Task: {task}
-Response:`
-
-  
-  export { planPrompt, solvePrompt};
+Response:`;
+export { planPrompt, solvePrompt };
--- src/tools.ts
+++ src/tools.ts
@@ -1,65 +1,156 @@
-import { ToolDefinition } from "@langchain/core/language_models/base";
+/** @format */
 
+import { ToolDefinition } from "@langchain/core/language_models/base";
 const calculatorTool: ToolDefinition = {
-    type: "function",
-    function: {
-      name: "calculate",
-      description: "Perform basic arithmetic operations on two numbers, including powers and roots",
-      parameters: {
-        type: "object",
-        properties: {
-          a: {
-            type: "number",
-            description: "The first operand",
-          },
-          b: {
-            type: "number",
-            description: "The second operand (For roots, this is the degree of the root)",
-          },
-          operator: {
+  type: "function",
+  function: {
+    name: "calculate",
+    description:
+      "Perform basic arithmetic operations on two numbers, including powers and roots",
+    parameters: {
+      type: "object",
+      properties: {
+        a: {
+          type: "number",
+          description: "The first operand",
+        },
+        b: {
+          type: "number",
+          description:
+            "The second operand (For roots, this is the degree of the root)",
+        },
+        operator: {
+          type: "string",
+          enum: ["add", "subtract", "multiply", "divide", "power", "root"],
+          description:
+            "The arithmetic operation to perform. For 'power', 'a' is raised to the power of 'b'. For 'root', it calculates the 'b'th root of 'a'.",
+        },
+      },
+      required: ["a", "b", "operator"],
+    },
+  },
+};
+const calculatorToolPlannerDescription =
+  "calculate[operation] Perform basic arithmetic operations on two numbers, including powers and roots";
+
+const sqlQuery: ToolDefinition = {
+  type: "function",
+  function: {
+    name: "createSQLquery",
+    description:
+      "Creates a given SQL query with specified parameters and returns the result set.The table's columns definition is provided.",
+    parameters: {
+      type: "object",
+      properties: {
+        sql: {
+          type: "string",
+          description:
+            "The SQL query string to be executed. Can include parameters. Never use HAVING statement, but WHERE.",
+        },
+        sql_insert: {
+          type: "string",
+          description:
+            "The SQL query string to be executing, containing the CREATE TABLE statement based on the 'sql' property, the table columns definition should be generated based on the desired result input by the user.",
+        },
+        chart: {
+          type: "boolean",
+          description:
+            "Indicate if based on the generated query, a chart would be helpful to understand better the data.",
+        },
+        // params: {
+        //   type: "array",
+        //   description:
+        //     "An array of parameters to be injected into the SQL query. These parameters correspond to placeholders in the 'sql' string.",
+        //   items: {
+        //     type: "string",
+        //     description:
+        //       "A parameter value to be safely injected into the SQL query",
+        //   },
+        // },
+      },
+      required: ["sql", "sql_insert", "chart"],
+    },
+  },
+};
+
+const segmentTool: ToolDefinition = {
+  type: "function",
+  function: {
+    name: "addSegment",
+    description:
+      "Creates a given SQL query with specified parameters and returns the result set",
+    parameters: {
+      type: "object",
+      properties: {
+        name: {
+          type: "string",
+          description: "Name of the segment",
+        },
+        created_by: {
+          type: "string",
+          description: "Name of the user that executed the query",
+        },
+        last_edited: {
+          type: "string",
+          description:
+            "Actual date of the function execution in format YYYY-MM-DD",
+        },
+        condition: {
+          type: "array",
+          items: {
             type: "string",
-            enum: ["add", "subtract", "multiply", "divide", "power", "root"],
-            description: "The arithmetic operation to perform. For 'power', 'a' is raised to the power of 'b'. For 'root', it calculates the 'b'th root of 'a'.",
+            enum: ["age", "amount", "gender", "category", "product"],
           },
         },
-        required: ["a", "b", "operator"],
+        table_name: {
+          type: "string",
+          description:
+            "Table name that was used to insert data from the createSQLquery function.",
+        },
       },
+      required: [
+        "name",
+        "created_by",
+        "last_edited",
+        "condition",
+        "table_name",
+      ],
     },
-  };
-
-const calculatorToolPlannerDescription = "calculate[operation] Perform basic arithmetic operations on two numbers, including powers and roots";
+  },
+};
 
 const emailTool: ToolDefinition = {
   type: "function",
   function: {
-    name: "createEmailTemplate",
-    description: "Creates an email for a campaign based on the template_name, the subject and message",
+    name: "createEmail",
+    description:
+      "Creates an email for a campaign based on the template_name, the subject and message",
     parameters: {
       type: "object",
-        properties: {
-          template_name: {
-            type: "string",
-            description: "Name of the email template",
-          },
-          subject: {
-            type: "string",
-            description: "Subject of the email",
-          },
-          message: {
-            type: "string",
-            description: "Body of the email to be sent",
-          },
+      properties: {
+        template_name: {
+          type: "string",
+          description: "Name of the email template",
         },
-      required: ["template_name", "subject", "message"]
-    }
-  }
+        subject: {
+          type: "string",
+          description: "Subject of the email",
+        },
+        message: {
+          type: "string",
+          description: "Body of the email to be sent",
+        },
+      },
+      required: ["template_name", "subject", "message"],
+    },
+  },
 };
-
 const eventTool: ToolDefinition = {
   type: "function",
   function: {
     name: "createEvent",
-    description: "Creates an event trigger for a campaign based on specified type and value",
+    description:
+      "Creates an event trigger for a campaign based on specified type and value",
     parameters: {
       type: "object",
       properties: {
@@ -78,20 +169,19 @@ const eventTool: ToolDefinition = {
             "SocialComment",
             "SocialPostHashtag",
             "SocialSharePost",
-            "SocialLikePicture"
+            "SocialLikePicture",
           ],
-          description: "Event type details"
+          description: "Event type details",
         },
         eventValue: {
           type: "string",
-          description: "The specific event"
-        }
+          description: "The specific event",
+        },
       },
-      required: ["eventType", "eventValue"]
-    }
-  }
+      required: ["eventType", "eventValue"],
+    },
+  },
 };
-
 const filterTool: ToolDefinition = {
   type: "function",
   function: {
@@ -102,47 +192,59 @@ const filterTool: ToolDefinition = {
       properties: {
         criteria: {
           type: "array",
-          description: "List of filter criteria",
           items: {
-            type: "object",
-            enum: ["age", "purchaseAmount", "gender"],
-            properties: {
-              criterionType: {
-                type: "string",
-                description: "Filter properties"
-              },
-              details: {
-                type: "object",
-                additionalProperties: true,
-                description: "Details of the criterion, structure depends on criterionType"
-              }
-            },
-            required: ["criterionType", "details"]
-          }
-        }
+            type: "string",
+            enum: ["age", "amount", "gender", "category", "product"],
+          },
+        },
+        value: {
+          type: "array",
+          items: {
+            type: "string",
+          },
+          description:
+            "An array containing the value of the criteria that are going to be applied to the campaign. For comparison types, use these ones <x, >x, =, =<, =>, <x> as needed.",
+        },
       },
-      required: ["criteria"]
-    }
-  }
+      required: ["criteria", "value"],
+    },
+  },
 };
-
 const rewardTool: ToolDefinition = {
   type: "function",
   function: {
     name: "createReward",
-    description: "Creates a reward for a marketing campaign",
+    description:
+      "Creates a reward for a marketing campaign, allowing for various reward types including coupons for fixed amounts or percentages, products, or points.",
     parameters: {
       type: "object",
       properties: {
         rewardType: {
           type: "string",
           enum: ["coupon", "product", "points"],
-          description: "Type of reward (coupon, product, or points)"
+          description: "Type of reward (coupon, product, or points)",
         },
-        amount: {
-          type: "string",
-          enum: ["discount", "fixedAmount"],
-          description: "Fixed amount or discount percentage"
+        details: {
+          type: "array",
+          items: {
+            type: "object",
+            properties: {
+              type: {
+                type: "string",
+                enum: ["fixedAmount", "percentage", "productID", "points"],
+                description:
+                  "Specifies the reward detail type: fixed amount, percentage for coupons; product ID for products; or a points value.",
+              },
+              value: {
+                type: "string",
+                description:
+                  "The value associated with the reward detail, such as the coupon value, product ID, or points amount.",
+              },
+            },
+            required: ["type", "value"],
+          },
+          description:
+            "An array containing the details of the reward, accommodating various types and values.",
         },
         validity: {
           type: "object",
@@ -150,30 +252,212 @@ const rewardTool: ToolDefinition = {
             start: {
               type: "string",
               format: "date",
-              description: "Start date of the reward's validity"
+              description: "Start date of the reward's validity",
             },
             end: {
               type: "string",
               format: "date",
-              description: "End date of the reward's validity"
-            }
+              description: "End date of the reward's validity",
+            },
           },
-          required: ["start", "end"]
-        }
+          required: ["start", "end"],
+        },
       },
-      required: ["rewardType", "amount", "validity"]
-    }
-  }
+      required: ["rewardType", "details", "validity"],
+    },
+  },
 };
 
-const campaginCreatorDescription = "createCampaing[campaing requirements] Description of the campaign requirements, it returns true if the campaign is created successfully, otherwise it returns false"; // TODO: improve this explaining the required fields and update the prommpt example
+const tableTool: ToolDefinition = {
+  type: "function",
+  function: {
+    name: "createTableStructure",
+    description:
+      "Generates a PostgreSQL CREATE TABLE statement from a JSON array representing CSV data. The tool infers and returns the data types for each column, based on the json provided. The result will be returned in this format for the headers: column_name type (int, text, etc), column_name2 type (int, text, etc), and the table name as a string. Don't respond until you have the type of the column following the column name.",
+    parameters: {
+      type: "object",
+      properties: {
+        columns: {
+          type: "array",
+          description:
+            "The columns for the table, representing the column headers. The data types are to be shouwn after each column name.",
+          items: {
+            type: "string",
+          },
+        },
+        tableName: {
+          type: "string",
+          description:
+            "The name of the table to be created, without schema prefix.",
+        },
+      },
+      required: ["columns", "tableName"],
+    },
+  },
+};
 
+const pageHtmlTool: ToolDefinition = {
+  type: "function",
+  function: {
+    name: "updateHtml",
+    description:
+      "Given an html code, returns a different element's organization.",
+    parameters: {
+      type: "object",
+      properties: {
+        html: {
+          type: "string",
+          description: "The updated html code returned.",
+        },
+      },
+      required: ["html"],
+    },
+  },
+};
 
-const toolsDescriptions = [calculatorToolPlannerDescription, campaginCreatorDescription];
+const organizeItemTool: ToolDefinition = {
+  type: "function",
+  function: {
+    name: "organizeItems",
+    description: "An array of items, organize them as the user ask.",
+    parameters: {
+      type: "object",
+      properties: {
+        cards: {
+          type: "array",
+          description: "The organized array of elements.",
+          items: { type: "string" },
+        },
+      },
+      required: ["cards"],
+    },
+  },
+};
 
+const chartTool: ToolDefinition = {
+  type: "function",
+  function: {
+    name: "createChart",
+    description:
+      "Generates a chart based on provided labels, data, and chart type",
+    parameters: {
+      type: "object",
+      properties: {
+        labels: {
+          type: "array",
+          description:
+            "The labels for the chart, usually representing the X-axis or categories",
+          items: { type: "string" },
+        },
+        data: {
+          type: "array",
+          description:
+            "The data points for the chart, corresponding to the labels",
+          items: { type: "number" },
+        },
+        chartType: {
+          type: "string",
+          description:
+            "The type of chart to generate (e.g., 'bar', 'line', 'doughnut')",
+        },
+      },
+      required: ["labels", "data", "chartType"],
+    },
+  },
+};
 
+const infoCardTool: ToolDefinition = {
+  type: "function",
+  function: {
+    name: "createInfoCard",
+    description: "Generates a list of objects based on provided input data",
+    parameters: {
+      type: "object",
+      properties: {
+        title: {
+          type: "string",
+          description: "The title for the card",
+        },
+        data: {
+          type: "string",
+          description: "The data points for the card",
+        },
+        percentage: {
+          type: "string",
+          description: "The percentage value, if exists.",
+        },
+      },
+      required: ["title", "data", "percentage"],
+    },
+  },
+};
+
+const cardTool: ToolDefinition = {
+  type: "function",
+  function: {
+    name: "createCardSQLquery",
+    description:
+      "Creates a given SQL query with specified parameters and returns the result set.The table's columns definition is provided.",
+    parameters: {
+      type: "object",
+      properties: {
+        sql: {
+          type: "string",
+          description:
+            "The SQL query string to be executed. Can include parameters. Never use HAVING statement, but WHERE.",
+        },
+      },
+      required: ["sql"],
+    },
+  },
+};
+
+const campaignCreatorDescription =
+  "createCampaign[campaign description] Description of the campaign requirements, it returns true if the campaign is created successfully, otherwise it returns false. All 4 must should be used, emailTool, eventTool, filterTool, rewardTool."; // TODO: improve this explaining the required fields and update the prommpt example
+const createTableDescription =
+  "createTableStructure[jsonData, tableName] Creates and fills the table based on a provided JSON array. It automatically identifies column types and sanitizes data to ensure consistency and uniqueness. The table name is specified without any schema prefix. Returns true if the table was created successfully and the user is seeing table.";
+const createChartDescription =
+  "createChart[jsonLabel, jsonData, chartType] Extracts data from a JSON array to create arrays for chart labels and values. If the chart type is not specified, it determines the most suitable chart type ('bar', 'line', 'doughnut') based on the data characteristics. Designed for easy integration with JavaScript charting components.";
+const createSQLqueryDescription =
+  "createSQLquery[query description] Description of the desired filtered data.";
+const createCardSQLqueryDescription =
+  "createCardSQLquery[query description] Description of the desired filtered data.";
+const addSegmentDescription =
+  "addSegment[query description] Description of the desired data to be inserted in the segments table.";
+const createInfoCardDescription =
+  "createInfoCard[title, data, percentage] Creates an array of elements that will display data based on inputs.";
+const editHtmlDescription =
+  "updateHtml[html] Updates an existing html code for better organization.";
+const organizeItemsDescription =
+  "organizeItems[cards] Organize an array of items based on user's input requirements.";
+const toolsDescriptions = [
+  calculatorToolPlannerDescription,
+  campaignCreatorDescription,
+  createTableDescription,
+  createChartDescription,
+  createSQLqueryDescription,
+  addSegmentDescription,
+  createInfoCardDescription,
+  createCardSQLqueryDescription,
+  editHtmlDescription,
+  organizeItemsDescription,
+];
 function getAllToolsDescriptions() {
   return toolsDescriptions;
 }
-  
-  export { calculatorTool, emailTool, eventTool, filterTool, rewardTool, getAllToolsDescriptions };
+export {
+  calculatorTool,
+  emailTool,
+  eventTool,
+  filterTool,
+  rewardTool,
+  tableTool,
+  chartTool,
+  infoCardTool,
+  cardTool,
+  sqlQuery,
+  segmentTool,
+  pageHtmlTool,
+  organizeItemTool,
+  getAllToolsDescriptions,
+};
