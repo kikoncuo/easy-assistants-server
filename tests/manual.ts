5120555ed6081a26638e2100fdb3e87bb6c78a49
Enrique <enriqueag26@gmail.com>
2024-06-24 09:18:46 +0200

    updated manual test

--- tests/manual.ts
+++ tests/manual.ts
@@ -90,9 +90,9 @@ function promptUserInput() {
   }
   if (query == "db") {
     // If the query is empty, set it to the result of 3*6 divided by 2
-    query = "Create a graph to highlight my top 10 customers in terms of their TLV, my tables are Transactions, Users and Products"
-    Logger.log(`Using DB test. Create a graph to highlight my top 10 customers in terms of their TLV, my tables are Transactions, Users and Products`);
-  }
+    query = "give me my top 5 customers who bought the most products"
+    Logger.log(`Using DB test. give me my top 5 customers who bought the most products`);
+  } 
 
   if (ws) {
     Logger.time('planTimer'); // Start the timer
