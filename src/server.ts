75bb2959584ea1f19d8eecfb8f9629674c18f74b
Lluis Erena <lluis.erena@gmail.com>
2024-07-08 16:29:16 +0200

    Clean agent

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,7 @@ import http from 'http';
 import dotenv from 'dotenv';
 import Logger from './utils/Logger';
 import { saveCsvTable } from './services/DirectFlowHandler';
-import { createAgent } from './models/Models';
-import { createTableStructure } from './models/Tools';
-import { anthropicSonnet } from './models/Models';
+import { externalAgents } from './utils/ExternalAgents';
 
 dotenv.config();
 
@@ -118,12 +116,9 @@ wss.on('connection', ws => {
     }
     else if (data.type === 'csvLoader') {
       Logger.log('Processing task:', data.task);
+      const { agent, agentPrompt } = externalAgents.csvLoader;
       await saveCsvTable(
-        createAgent(anthropicSonnet(), [createTableStructure]),
-        `You are an LLM specialized in the entire process of transforming JSON data into a fully functional PostgreSQL. This is done by using your createTableStructure tool to create the table. This should return the column name followed by the data type of that column.
-          If you detect any date column return it as a type text.
-          Column names should never include whitespaces, but rather underscore for separating words, ensure there are no whitespaces in the items inside columns array.
-          Always respond using the tool`,
+        agent, agentPrompt,
         (type: string, functions: Array<{ function_name: string; arguments: any }>) =>
             WebSocketService.queryUser(type, functions, ws),
         data.task
