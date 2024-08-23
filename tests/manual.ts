45f41183c7379e4adffd8c04a98619e1f08ef564
Enrique <enriqueag26@gmail.com>
2024-08-23 14:17:05 +0200

    Add graph creation type to manual.ts

--- tests/manual.ts
+++ tests/manual.ts
@@ -14,7 +14,7 @@ function connectToServer() {
   
   ws.on('open', () => {
     Logger.log('Connected to server');
-    ws?.send(JSON.stringify({ type: 'configure', configData: [9] }));
+    ws?.send(JSON.stringify({ type: 'configure', configData: [9], appType: 'default' }));
     promptUserInput();
 
    /*ws?.send(JSON.stringify({ 
